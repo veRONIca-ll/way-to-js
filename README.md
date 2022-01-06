5f6781636f3780eb138135e8a1d278d1b6c8761e
“veronica <molcanova.v_qschool@qsoft.ru>
2022-01-07 02:15:02 +0300

    clicker.001 / unclick on div

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # way-to-js
-small projects to learn js
+there are small projects to learn js
+
+1. clicker-001 - a simple clicker 
+click on a button will add 1 to a clicker counter
+click everywhere except for button will subtract 1 from a clicker counter
++ you cannot go less than 0 
--- clicker-001/src/script.js
+++ clicker-001/src/script.js
@@ -1,5 +1,17 @@
 let count = 0;
 
+function outputCount(newCount) {
+    if (newCount < 0) {
+        newCount = 0;
+        alert('do not go less than zero');
+    }
+    document.getElementById('count').innerText = 'Clicked: ' + newCount;
+}
+
 function countClick() {
-    document.getElementById('count').innerText = 'Clicked: ' + (++count);
+    outputCount(++count);
+}
+
+function uncountClick() {
+    outputCount(--count);
 }
