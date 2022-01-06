ca09cd110c6a784579c0f2015cb390ea9f346fc0
“veronica <molcanova.v_qschool@qsoft.ru>
2022-01-07 01:07:47 +0300

    clicker.001 / small changes

--- clicker-001/src/script.js
+++ clicker-001/src/script.js
@@ -1,6 +1,5 @@
 let count = 0;
 
 function countClick() {
-    ++count;
-    document.getElementById('count').innerText = 'Clicked: ' + count;
+    document.getElementById('count').innerText = 'Clicked: ' + (++count);
 }
